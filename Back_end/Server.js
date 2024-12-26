463a048bd1e97c66117fe5048d15c7e5611c8f51
Anishkumar2000 <anishkumar8868@gmail.com>
2024-12-26 15:00:00 +0530

    Done

--- Back_end/Server.js
+++ Back_end/Server.js
@@ -1,13 +1,13 @@
 const express = require("express");
 const app = express();
-const router = require("./Router/router");
-require("./DB/connections");
+// const router = require("./Router/router");
+// require("./DB/connections");
 const cors = require("cors");
 const Port = 7001;
 
 app.use(cors({origin: "http://localhost:3000"}));
 app.use(express.json());
-app.use("/",router);
+// app.use("/",router);
 
 app.listen(Port,()=>{
     console.log("App is Running");
